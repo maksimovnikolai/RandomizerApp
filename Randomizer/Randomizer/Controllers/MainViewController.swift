b5bd11a774bd651893687e5db6b28182531c28df
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-20 02:38:28 +0300

    удален storyboard

---
+++ Randomizer/Randomizer/Controllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Randomizer
+//
+//  Created by Nikolai Maksimov on 20.12.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
