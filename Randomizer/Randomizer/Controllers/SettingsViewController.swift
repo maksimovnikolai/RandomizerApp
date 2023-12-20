8860662c9b0a94ae420a872ecfdb8e84c7a3e476
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-20 17:03:48 +0300

    добавлен экран настройки диапазона чисел

---
+++ Randomizer/Randomizer/Controllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Randomizer
+//
+//  Created by Nikolai Maksimov on 20.12.2023.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
